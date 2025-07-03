c273ae86e6d3e0598870317b60256d020c07f730
RAGHAV JUNEJA <raghavjuneja2391@gmail.com>
2025-07-03 21:36:49 +0530

    Added apps with built in categories

--- expenses/admin.py
+++ expenses/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User,Expense,ExpenseCategory
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ['email', 'first_name', 'last_name', 'is_active', 'created_at']
+    list_filter = ['is_active', 'is_staff', 'created_at']
+    search_fields = ['email', 'first_name', 'last_name']
+    ordering = ['-created_at']
+    
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('Timestamps', {'fields': ('created_at', 'updated_at')}),
+    )
+    readonly_fields = ['created_at', 'updated_at']
+@admin.register(ExpenseCategory)
+class ExpenseCategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'created_at']
+    search_fields = ['name']
+    ordering = ['name']
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ['user', 'amount', 'category', 'date', 'created_at']
+    list_filter = ['category', 'date', 'created_at']
+    search_fields = ['user__email', 'description']
+    date_hierarchy = 'date'
+    ordering = ['-date', '-created_at']
+    
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('user', 'category')
